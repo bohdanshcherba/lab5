cc146dc957acc4e7368e371d4d25d8db68af7958
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2020-12-11 16:19:27 +0200

    Update Cineme.h
    update after review

--- Cineme.h
+++ Cineme.h
@@ -1,73 +1,67 @@
-
 #include "Movie.h"
+class Cinema{
+    private:
+        vector<Movie> movies;
+        string name;
+        
+        int seats;
 
-
-class Cinema
-{
-private:
-    vector<Movie> movies;
-    string name;
-    
-    int seats;
-
-public:
-    void printCinema()
-    {
-        cout << "Cinema: " << name << ";" << endl
-             << endl;
-    }
-    Cinema(string tmpName, int tmpSeats)
-    {
-        name = tmpName;
-
-        seats = tmpSeats;
-    }
-
-    void addNewMovieToCinema(Movie movie)
-    {
-        movies.push_back(movie);
-    }
-
-    void filterMovieByGenre(type genreFilter)
-    {
-        for (int i = 0; i < movies.size(); i++)
+    public:
+        void printCinema()
         {
-            if (movies.at(i).getGenre() == genreFilter)
-            {
-                movies.at(i).print();
-            }
+            cout << "Cinema: " << name << ";" << endl
+                << endl;
         }
-    }
-    
-    void printAllMovies()
-    {
-        for (int i = 0; i < movies.size(); i++)
+        Cinema(string tmpName, int tmpSeats)
         {
-            movies.at(i).print();
+            name = tmpName;
+
+            seats = tmpSeats;
         }
 
-        cout << "Count of movies : " << movies.size() << endl;
-    }
-
-    void sortMoviesByRank()
-    {
-        Movie entry;
-        for (int i = 0; i < movies.size(); i++)
+        void addNewMovieToCinema(Movie movie)
         {
-            for (int j = 0; j < movies.size() - i - 1; j++)
+            movies.push_back(movie);
+        }
+
+        void filterMovieByGenre(type genreFilter)
+        {
+            for (int i = 0; i < movies.size(); i++)
             {
-                if (movies.at(j).getRanking() > movies.at(j + 1).getRanking())
+                if (movies.at(i).getGenre() == genreFilter)
                 {
-                    entry = movies.at(j);
-                    movies.at(j) = movies.at(j + 1);
-                    movies.at(j + 1) = entry;
+                    movies.at(i).print();
                 }
             }
         }
-    }
+        
+        void printAllMovies()
+        {
+            for (int i = 0; i < movies.size(); i++)
+            {
+                movies.at(i).print();
+            }
+
+            cout << "Count of movies : " << movies.size() << endl;
+        }
+
+        void sortMoviesByRank()
+        {
+            Movie entry;
+            for (int i = 0; i < movies.size(); i++)
+            {
+                for (int j = 0; j < movies.size() - i - 1; j++)
+                {
+                    if (movies.at(j).getRanking() > movies.at(j + 1).getRanking())
+                    {
+                        entry = movies.at(j);
+                        movies.at(j) = movies.at(j + 1);
+                        movies.at(j + 1) = entry;
+                    }
+                }
+            }
+        }
 };
-
-
 
 tm MakeDate(int day, int month, int year)
 {
@@ -78,17 +72,13 @@ tm MakeDate(int day, int month, int year)
     return date;
 }
 
-class Entry
-{
-public:
-    tm date;
-    string name;
-
-public:
-    Entry() {}
-    Entry(string name, tm date) : name(name), date(date) {}
+class Entry{
+    public:
+        tm date;
+        string name;
+        Entry() {}
+        Entry(string name, tm date) : name(name), date(date) {}
 };
-
 
 bool CompareDates(tm d1, tm d2)
 {
