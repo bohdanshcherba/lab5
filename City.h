99e82781efc4835390d8a84790011728180ea68c
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2020-12-11 16:19:30 +0200

    Update City.h
    update after review

--- City.h
+++ City.h
@@ -1,31 +1,31 @@
 #include "Area.h"
 class City{
     private:
-    vector<Area> areas;
-    string nameCity;
+        vector<Area> areas;
+        string nameCity;
     public:
-    void printCity(){
-        cout << "City:" << nameCity << endl;
-    }
-
-    City(string tmpName){
-        nameCity = tmpName;
-    }
-
-    void addArea(Area  area){
-        areas.push_back(area);
-    }
-
-    void printAllArea()
-    {
-        for (int i = 0; i < areas.size(); i++)
-        {
-            cout << i+1 <<".";
-            areas.at(i).printArea();
+        void printCity(){
+            cout << "City:" << nameCity << endl;
         }
 
-        cout << "Count of area: " << areas.size() << endl;
-    }
+        City(string tmpName){
+            nameCity = tmpName;
+        }
+
+        void addArea(Area  area){
+            areas.push_back(area);
+        }
+
+        void printAllArea()
+        {
+            for (int i = 0; i < areas.size(); i++)
+            {
+                cout << i+1 <<".";
+                areas.at(i).printArea();
+            }
+
+            cout << "Count of area: " << areas.size() << endl;
+        }
 
 };
 
@@ -33,25 +33,25 @@ class City{
 
 class Contry{
     private:
-    vector<City> cities;
-    string name;
+        vector<City> cities;
+        string name;
     public:
-    Contry(string tmpName){
-        name = tmpName;
-    }
-    void addCity(City name){
-        cities.push_back(name);
-    }
-
-    void printAllCity()
-    {
-        for (int i = 0; i < cities.size(); i++)
-        {
-            cout << i+1 <<".";
-            cities.at(i).printCity();
+        Contry(string tmpName){
+            name = tmpName;
+        }
+        void addCity(City name){
+            cities.push_back(name);
         }
 
-        cout << "Count of City: " << cities.size() << endl;
-    }
+        void printAllCity()
+        {
+            for (int i = 0; i < cities.size(); i++)
+            {
+                cout << i+1 <<".";
+                cities.at(i).printCity();
+            }
+
+            cout << "Count of City: " << cities.size() << endl;
+        }
 
 };
