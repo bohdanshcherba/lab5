1c85086bc2d435bf92ee19f2aa5fde891783e31f
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2020-12-11 16:19:22 +0200

    Update Area.h
    update after review

--- Area.h
+++ Area.h
@@ -2,25 +2,25 @@
 
 class Area{
     private:
-    vector<Cinema> cinema;
-    string nameArea;
+        vector<Cinema> cinema;
+        string nameArea;
     
     public:
-    Area(string tmpNameArea){
-        nameArea = tmpNameArea;
-    }
-    void addCinema(Cinema name){
-        cinema.push_back(name);
-    }
-    void printArea(){
-        cout<<"Area:" << nameArea<<endl; 
-    }
-    void printAllCinema()
-    {
-        for (int i = 0; i < cinema.size(); i++){
-            cout << i+1 <<".";
-            cinema.at(i).printCinema();
+        Area(string tmpNameArea){
+            nameArea = tmpNameArea;
         }
-        cout << "Count of Cinema: " << cinema.size() << endl;
-    }
+        void addCinema(Cinema name){
+            cinema.push_back(name);
+        }
+        void printArea(){
+            cout<<"Area:" << nameArea<<endl; 
+        }
+        void printAllCinema()
+        {
+            for (int i = 0; i < cinema.size(); i++){
+                cout << i+1 <<".";
+                cinema.at(i).printCinema();
+            }
+            cout << "Count of Cinema: " << cinema.size() << endl;
+        }
 };
