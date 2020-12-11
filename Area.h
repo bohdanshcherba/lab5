f1f3ced3926b128374c0886da86151bd52558f65
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2020-12-11 16:15:14 +0200

    Update Area.h
    update after review

--- Area.h
+++ Area.h
@@ -1,6 +1,4 @@
-
 #include "Cineme.h"
-
 
 class Area{
     private:
@@ -11,24 +9,18 @@ class Area{
     Area(string tmpNameArea){
         nameArea = tmpNameArea;
     }
-
     void addCinema(Cinema name){
         cinema.push_back(name);
     }
-
     void printArea(){
         cout<<"Area:" << nameArea<<endl; 
-
     }
-
     void printAllCinema()
     {
-        for (int i = 0; i < cinema.size(); i++)
-        {
+        for (int i = 0; i < cinema.size(); i++){
             cout << i+1 <<".";
             cinema.at(i).printCinema();
         }
-
         cout << "Count of Cinema: " << cinema.size() << endl;
     }
 };
